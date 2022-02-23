accc4ade65a9ae9570b33c95dc9fa9e8520eeb3a
Daniel Hines <daniel.c.hines@vanderbilt.edu>
2022-02-23 12:47:50 -0600

    using screenshot

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,11 @@ const Home: NextPage = () => {
         <meta name="theme-color" content="#000000" />
         <meta property="og:site_title" content="Reddit Time Machine" />
         <meta property="og:title" content="Reddit Time Machine" />
-        <meta name="og:author" content="Danny Hines" />
+        <meta property="og:author" content="Danny Hines" />
+        <meta
+          property="description"
+          content="View the most popular news, discussions and memes from Reddit on a day in the past."
+        />
         <meta
           property="og:description"
           content="View the most popular news, discussions and memes from Reddit on a day in the past."
@@ -21,7 +25,7 @@ const Home: NextPage = () => {
         <meta property="og:type" content="website" />
         <meta
           property="og:image"
-          content="https://random-public-images-dch.s3.amazonaws.com/rtm-logo.png"
+          content="https://random-public-images-dch.s3.amazonaws.com/rtm-screenshot.png"
         />
         <meta
           name="keywords"
@@ -33,10 +37,6 @@ const Home: NextPage = () => {
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:text:title" content="Reddit Time Machine" />
         <meta name="twitter:creator" content="@danny__hines" />
-        <meta
-          name="twitter:image"
-          content="https://random-public-images-dch.s3.amazonaws.com/rtm-screenshot.png"
-        />
       </Head>
 
       <Header />
