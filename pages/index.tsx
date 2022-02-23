b27b0b52d67d105ad6737ae46f36c204ca8b98c2
Daniel Hines <daniel.c.hines@vanderbilt.edu>
2022-02-23 01:21:04 -0600

    twitter card title

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,7 @@ const Home: NextPage = () => {
       <Head>
         <title>Reddit Time Machine</title>
         <meta name="theme-color" content="#000000" />
+        <meta name="title" content="Reddit Time Machine" />
         <meta
           name="description"
           content="View the most popular news, discussions and memes from Reddit on a day in the past."
@@ -22,13 +23,13 @@ const Home: NextPage = () => {
         <meta name="author" content="Danny Hines" />
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
+        {/*   Twitter   */}
         <meta name="twitter:card" content="app" />
         <meta
           name="twitter:description"
           content="View the most popular news, discussions and memes from Reddit on a day in the past."
         />
-        <meta name="twitter:title" content="Reddit Time Machine" />
+        <meta name="twitter:text:title" content="Reddit Time Machine" />
         <meta
           name="twitter:image"
           content="https://random-public-images-dch.s3.amazonaws.com/rtm-logo.png"
