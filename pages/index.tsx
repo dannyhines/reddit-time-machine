285d9f809af41b0eba4ed9c013987f4726d796e7
Daniel Hines <daniel.c.hines@vanderbilt.edu>
2022-02-23 11:59:17 -0600

    OG meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,10 +11,17 @@ const Home: NextPage = () => {
       <Head>
         <title>Reddit Time Machine</title>
         <meta name="theme-color" content="#000000" />
-        <meta name="title" content="Reddit Time Machine" />
+        <html prefix="og: http://ogp.me/ns#"></html>
+        <meta property="og:site_title" content="Reddit Time Machine" />
+        <meta property="og:title" content="Reddit Time Machine" />
         <meta
-          name="description"
+          property="og:description"
           content="View the most popular news, discussions and memes from Reddit on a day in the past."
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://random-public-images-dch.s3.amazonaws.com/rtm-logo.png"
         />
         <meta
           name="keywords"
@@ -25,15 +32,7 @@ const Home: NextPage = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         {/*   Twitter   */}
         <meta name="twitter:card" content="app" />
-        <meta
-          name="twitter:description"
-          content="View the most popular news, discussions and memes from Reddit on a day in the past."
-        />
         <meta name="twitter:text:title" content="Reddit Time Machine" />
-        <meta
-          name="twitter:image"
-          content="https://random-public-images-dch.s3.amazonaws.com/rtm-logo.png"
-        />
         <meta name="twitter:creator" content="@danny__hines" />
       </Head>
 
