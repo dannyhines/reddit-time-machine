88e7b61cac1f5dd1a28ea383eddb711d130a641b
Daniel Hines <daniel.c.hines@vanderbilt.edu>
2022-08-07 10:08:42 -0400

    center logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
-import React from "react";
-import styles from "../styles/Home.module.css";
+import Image from 'next/image';
+import React from 'react';
+import styles from '../styles/Home.module.css';
 
 interface HeaderProps {}
 
@@ -8,9 +8,9 @@ const Header: React.FC<HeaderProps> = (props) => {
   return (
     <header className={styles.app_header}>
       <div className={styles.app_logo}>
-        <Image src="/reddit-icon.png" height={45} width={45} alt="Logo" />
+        <Image src='/reddit-icon.png' height={45} width={45} alt='Logo' />
       </div>
-      <h1 style={{ fontSize: 24 }}>Reddit Time Machine</h1>
+      <h1 style={{ fontSize: 24, margin: 0 }}>Reddit Time Machine</h1>
     </header>
   );
 };
