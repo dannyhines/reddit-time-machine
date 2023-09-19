111ee72df8f480fba259a62a6abb7e75b4654dc9
Daniel Hines <daniel.c.hines@vanderbilt.edu>
2023-09-18 22:03:36 -0400

    new post type

--- types/Post.ts
+++ types/Post.ts
@@ -1,21 +1,53 @@
+// export interface Post {
+//   id: string;
+//   title: string;
+//   author: string;
+//   full_link: string;
+//   url: string;
+//   subreddit: string;
+//   score: number;
+//   preview?: ImagePreview;
+//   thumbnail?: string;
+//   thumbnail_height?: number;
+//   thumbnail_width?: number;
+//   created_utc: number;
+// }
+
 export interface Post {
   id: string;
   title: string;
-  author: string;
-  full_link: string;
   url: string;
-  subreddit: string;
-  score: number;
-  preview?: ImagePreview;
-  thumbnail?: string;
-  thumbnail_height?: number;
-  thumbnail_width?: number;
   created_utc: number;
+  author: string;
+  domain: string;
+  hidden: boolean;
+  score: number;
+  ups: number | null;
+  downs: number | null;
+  is_reddit_media_domain: boolean;
+  is_video: boolean;
+  num_comments: number;
+  num_crossposts: number | null;
+  permalink: string;
+  preview: Preview;
+  subreddit: string;
+  subreddit_id: string;
+  thumbnail: string | null;
+  thumbnail_height: number | null;
+  thumbnail_width: number | null;
+  created_date: string; // YYYY-MM-DD
+  post_type: "meme" | "news" | "politics" | "sports" | "science" | "pics";
 }
 
-interface ImagePreview {
-  images: { resolutions: Resolution[] };
+interface Preview {
+  enabled: boolean;
+  images: PreviewImage[];
+}
+
+interface PreviewImage {
+  resolutions: Resolution[];
   source: Resolution;
+  variants?: { [variant: string]: PreviewImage[] }[];
 }
 
 interface Resolution {
