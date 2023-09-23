3be6424107d2c3d226fbd2c56ae9fd9c13e3472b
Daniel Hines <daniel.c.hines@vanderbilt.edu>
2023-09-23 11:31:56 -0400

    image formatting

--- components/ImageCard.tsx
+++ components/ImageCard.tsx
@@ -18,31 +18,32 @@ interface CardViewProps {
 const LoadingCard = () => {
   const { isDesktop } = useWindowDimensions();
   const imgWidth = isDesktop ? 300 : 140;
-  const imgHeight = isDesktop ? 200 : 120;
+  const imgHeight = isDesktop ? 240 : 120;
   return (
     <Card style={{ maxWidth: 400, margin: "16px 0" }} bodyStyle={{ padding: 12 }}>
       <div style={{ width: "100%", opacity: 0.7 }}>
         <Skeleton.Image style={{ width: imgWidth, height: imgHeight }} />
       </div>
+      <br />
       <Meta description={<Skeleton active />} />
     </Card>
   );
 };
 
 const ImageCard: React.FC<CardViewProps> = (props) => {
-  const { isMobile } = useWindowDimensions();
   const { post, maxWidth, loading } = props;
+  const { isMobile } = useWindowDimensions();
+
   const { imgUrl, thumbnailUrl, imgUrlSmall, aspectRatio } = getImageUrls(post);
   const imgState = useImage(post ? imgUrl : undefined);
 
   if (!post || loading) {
     return <LoadingCard />;
   }
-  /* eslint-disable */
-  if (imgState.hasError) return null;
-  // if (imgState.hasStartedInitialFetch && !imgState.hasLoaded) {
-  //   return <LoadingCard />;
-  // }
+
+  if (imgState.hasError) {
+    return null;
+  }
 
   const titleStyle = (smallFont: number) => {
     return { fontSize: maxWidth < 300 ? smallFont : 14, margin: maxWidth < 300 ? 0 : 4, color: "inherit" };
@@ -52,33 +53,28 @@ const ImageCard: React.FC<CardViewProps> = (props) => {
     return { fontSize: maxWidth < 300 ? smallFont : 12, margin: maxWidth < 300 ? 0 : "0 0 0 8px", color: "inherit" };
   };
 
-  // const imgResolutions = post.preview?.images[0].resolutions ?? [];
-  // const imgSource = post.preview?.images[0].source;
-  // const thumbnailUrl = post.thumbnail && post.thumbnail !== "default" ? post.thumbnail : undefined;
-  // const smallestPreviewUrl = imgResolutions && imgResolutions.length ? imgResolutions[0].url : undefined;
-  // const placeholderUrl = smallestPreviewUrl ?? thumbnailUrl;
-
   return (
     <Card
       hoverable
-      style={{ maxWidth, maxHeight: 540, margin: "16px 0" }}
+      style={{ maxWidth, margin: "16px 0" }}
       cover={
         <Image
           alt={post.title}
-          src={isMobile ? imgUrlSmall ?? thumbnailUrl ?? imgUrl : imgUrl}
-          style={{ height: "auto", maxWidth }}
+          src={isMobile ? imgUrlSmall ?? imgUrl : imgUrl}
+          style={{ maxWidth, aspectRatio }}
           width='100%'
           // height={Math.min(imgSource.height, 420)}
-          height='auto'
+          // height='100%'
           placeholder={
             imgUrlSmall ? (
               <Image
                 preview={false}
-                src={imgUrlSmall}
+                src={imgUrlSmall ?? "default"}
                 // width={post.thumbnail_width + "px"}
                 width='100%'
                 // height={imgSource.height + "px"}
-                height='auto'
+                // height='100%'
+                alt='Image'
               />
             ) : null
           }
@@ -94,8 +90,8 @@ const ImageCard: React.FC<CardViewProps> = (props) => {
         onClick={() => sendLinkClickToGA("reddit", REDDIT_BASE_URL + post.permalink)}
       >
         <Meta
-          title={<p style={titleStyle(11)}>{post.title}</p>}
-          description={<p style={subtitleStyle(10)}>{`r/${post.subreddit} · ${post.score} pts`}</p>}
+          title={<p style={titleStyle(10)}>{post.title}</p>}
+          description={<p style={subtitleStyle(8)}>{`r/${post.subreddit} · ${post.score} pts`}</p>}
         />
       </a>
     </Card>
--- utils/getImageUrls.ts
+++ utils/getImageUrls.ts
@@ -1,29 +1,36 @@
 import { Post } from "../types/Post";
 
+const getAspectRatio = (width: number | null, height: number | null) => {
+  return width && height ? width / height : undefined;
+};
+
 /*
  *
- * returns the src & thumbnail images, calculates w/h (aspect ratio)
+ * returns the image urls, calculates w/h (aspect ratio)
+ * older posts don't contain a 'preview' field, thumbnail width & height
  * // TODO: use is_video, is_reddit_media_domain, preview.variants?
  */
 export function getImageUrls(post?: Post) {
   if (!post || !post.url) return { src: "" };
   let { url, thumbnail, preview, thumbnail_width, thumbnail_height } = post;
-  let aspectRatio = (thumbnail_width ?? 400) / (thumbnail_height ?? 300);
+  let aspectRatio = getAspectRatio(thumbnail_width, thumbnail_height);
 
-  url = url.replace("http://", "https://") ?? "";
   thumbnail = thumbnail && thumbnail !== "default" ? thumbnail : null; // most are 'default'
-  let smallestPreviewUrl = thumbnail;
+  let imgUrl = url.replace("http://", "https://") ?? "";
+  let smallestPreviewUrl = null; // tried to set to thumbnail, but it's too small
 
   if (preview) {
+    // If there's a 'preview' field, use that
     const imgObject = preview.images[0];
     const { source, resolutions } = imgObject;
-    aspectRatio = source.width / source.height;
+    imgUrl = source ? source.url : imgUrl;
+    aspectRatio = getAspectRatio(source.width, source.height) ?? aspectRatio;
     smallestPreviewUrl = resolutions && resolutions.length ? resolutions[0].url : null;
   }
 
   return {
-    imgUrl: url,
-    thumbnailUrl: thumbnail,
+    imgUrl,
+    thumbnailUrl: thumbnail ?? "default_thumbnail.png",
     imgUrlSmall: smallestPreviewUrl,
     aspectRatio,
   };
