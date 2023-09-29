7dcb8d2affeb817359c81183d2cecc59419d261b
Daniel Hines <daniel.c.hines@vanderbilt.edu>
2023-09-29 00:03:54 -0400

    smaller titles

--- components/ListTitle.tsx
+++ components/ListTitle.tsx
@@ -9,7 +9,7 @@ const ListTitle: React.FC<Props> = (props) => {
   const { isMobile } = useWindowDimensions();
   return (
     <div style={{ textAlign: "left" }}>
-      <h2 style={{ color: "#8c8c8c", fontSize: isMobile ? 16 : 20 }}>{props.children}</h2>
+      <h2 style={{ color: "#8c8c8c", fontSize: isMobile ? 14 : 18 }}>{props.children}</h2>
     </div>
   );
 };
