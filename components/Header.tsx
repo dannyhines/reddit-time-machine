321500362adeba511b054b3a4aa4e032d4e98d9e
Daniel Hines <daniel.c.hines@vanderbilt.edu>
2023-11-18 19:20:46 -0500

    header is link to home

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,21 @@
 import Image from "next/image";
 import React from "react";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 interface HeaderProps {}
 
 const Header: React.FC<HeaderProps> = (props) => {
   return (
     <header className={styles.app_header}>
-      <div className={styles.app_logo}>
-        <Image src='/reddit-icon.png' height={40} width={40} alt='Logo' />
-      </div>
-      <h1 style={{ fontSize: 24, margin: 0 }}>Reddit Time Machine</h1>
+      <Link href='/'>
+        <a className={styles.app_header_link}>
+          <div className={styles.app_logo}>
+            <Image src='/reddit-icon.png' height={40} width={40} alt='Logo' />
+          </div>
+          <h1 style={{ fontSize: 24, margin: 0 }}>Reddit Time Machine</h1>
+        </a>
+      </Link>
     </header>
   );
 };
